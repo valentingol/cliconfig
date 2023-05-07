c9bf8b3bc82d10db768044dccbd672fea919ff37
Valentin Goldité <valentin.goldite@gmail.com>
2023-05-07 18:10:03 +0200

    🚚 Rename parse > parse_cli

--- tests/test_cli_parser.py
+++ tests/test_cli_parser.py
@@ -2,18 +2,20 @@
 import pytest
 import pytest_check as check
 
-from cliconfig.cli_parser import parse
+from cliconfig.cli_parser import parse_cli
 
 
-def test_parse() -> None:
-    """Test for parse."""
-    config_paths, config_cli_params = parse(
+def test_parse_cli() -> None:
+    """Test for parse_cli."""
+    config_paths, config_cli_params = parse_cli(
         ["main.py", "--config", "config1.yaml,config2.yaml", "--a", "1", "--b=2"]
     )
     check.equal(config_paths, ["config1.yaml", "config2.yaml"])
     check.equal(config_cli_params, {"a": 1, "b": 2})
     b_str = "{'2': {'3': 4, '5': [6.3], '7': True, '8': Null, '9': [2, {'a': 1}]}}"
-    config_paths, config_cli_params = parse(["main.py", "--a='b=c'", f"--b={b_str}"])
+    config_paths, config_cli_params = parse_cli(
+        ["main.py", "--a='b=c'", f"--b={b_str}"]
+    )
     val = {
         "a": "b=c",
         "b": {"2": {"3": 4, "5": [6.3], "7": True, "8": None, "9": [2, {"a": 1}]}},
@@ -21,4 +23,4 @@ def test_parse() -> None:
     check.equal(config_paths, [])
     check.equal(config_cli_params, val)
     with pytest.raises(ValueError, match="Only one '--config ' argument is allowed.*"):
-        parse(["main.py", "--config", "config1.yaml", "--config", "config2.yaml"])
+        parse_cli(["main.py", "--config", "config1.yaml", "--config", "config2.yaml"])
