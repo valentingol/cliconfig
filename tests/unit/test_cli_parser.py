09cac0a4ba9d0d249284301424301b560435a884
Valentin Goldité <valentin.goldite@gmail.com>
2023-05-11 14:04:07 +0200

    ✅ Cover unknwon key in CLI

--- tests/unit/test_cli_parser.py
+++ tests/unit/test_cli_parser.py
@@ -8,7 +8,11 @@ from cliconfig.cli_parser import parse_cli
 def test_parse_cli() -> None:
     """Test for parse_cli."""
     config_paths, config_cli_params = parse_cli(
-        ["main.py", "--config", "config1.yaml,config2.yaml", "--a", "1", "--b=2"]
+        ["main.py", "--config", "config1.yaml,config2.yaml", "--a=1", "--b=2"]
+    )
+    check.equal(config_paths, ["config1.yaml", "config2.yaml"])
+    config_paths, config_cli_params = parse_cli(
+        ["main.py", "--config", "[config1.yaml, config2.yaml]", "--a=1", "--b=2"]
     )
     check.equal(config_paths, ["config1.yaml", "config2.yaml"])
     check.equal(config_cli_params, {"a": 1, "b": 2})
@@ -22,5 +26,11 @@ def test_parse_cli() -> None:
     }
     check.equal(config_paths, [])
     check.equal(config_cli_params, val)
+    config_paths, config_cli_params = parse_cli(
+        ["main.py", "--unknown", "--a=1", "--unknwon2=2", "--b=3"]
+    )
+    val2 = {"unknown": None, "a": 1, "unknwon2": 2, "b": 3}
+    check.equal(config_paths, [])
+    check.equal(config_cli_params, val2)
     with pytest.raises(ValueError, match="Only one '--config ' argument is allowed.*"):
         parse_cli(["main.py", "--config", "config1.yaml", "--config", "config2.yaml"])
