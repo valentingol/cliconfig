b6911e42200d7105a8f3cfa09908daf70187687c
Valentin Goldité <valentin.goldite@gmail.com>
2023-05-30 16:48:55 +0200

    ✨ Add feature to get, set, del params via config attributes

--- cliconfig/base.py
+++ cliconfig/base.py
@@ -10,7 +10,8 @@ class Config:
     """Class for configuration.
 
     Config object contain the config dict and the processing list
-    and no methods except ``__init__``, ``__repr__`` and ``__eq__``.
+    and no methods except ``__init__``, ``__repr__``, ``__eq__``,
+    ``__getattribute__``, ``__setattr__`` and ``__delattr__``.
     The Config objects are mutable and not hashable.
 
     Parameters
@@ -53,3 +54,47 @@ class Config:
                 equal = equal and processing in self.process_list
             return equal
         return False
+
+    def __getattribute__(self, __name: str) -> Any:
+        """Get attribute, sub-configuration or parameter.
+
+        The dict should be nested (unflattened). If it is not the case,
+        you can apply :func:`dict_routines.flatten` on ``config.dict``
+        to unflatten it.
+        """
+        if __name in ["dict", "process_list"]:
+            return super().__getattribute__(__name)
+        if __name not in self.dict:
+            keys = ", ".join(self.dict.keys())
+            raise AttributeError(  # pylint: disable=raise-missing-from
+                f"Config has no attribute '{__name}'. Available keys are: {keys}."
+            )
+        if isinstance(self.dict[__name], dict):
+            # If the attribute is a dict, return a Config object
+            # so that we can access the nested keys with multiple dots
+            return Config(self.dict[__name], process_list=self.process_list)
+        return self.dict[__name]
+
+    def __setattr__(self, __name: str, value: Any) -> None:
+        """Set attribute, sub-configuration or parameter.
+
+        The dict should be nested (unflattened). If it is not the case,
+        you can apply :func:`dict_routines.flatten` on ``config.dict``
+        to unflatten it.
+        """
+        if __name in ["dict", "process_list"]:
+            super().__setattr__(__name, value)
+        else:
+            self.dict[__name] = value
+
+    def __delattr__(self, __name: str) -> None:
+        """Delete attribute, sub-configuration or parameter.
+
+        The dict should be nested (unflattened). If it is not the case,
+        you can apply :func:`dict_routines.flatten` on ``config.dict``
+        to unflatten it.
+        """
+        if __name in ["dict", "process_list"]:
+            super().__delattr__(__name)
+        else:
+            del self.dict[__name]
