deef5271f4985c27f81bd01b53da9259c6ae9cda
valentingol <valentin.goldite@gmail.com>
2024-01-04 15:01:01 +0100

    📝 Change description

--- cliconfig/__init__.py
+++ cliconfig/__init__.py
@@ -9,21 +9,9 @@
 #    DISTRIBUTION and PRIVATE USE as long as the original license is
 #    include as well as this copy right notice.
 """
+# CLI Config.
+
 .. include:: ../DOCUMENTATION.md
-
-## License
-
-Copyright © 2023 Valentin Goldité
-
-This program is free software: you can redistribute it and/or modify it
-under the terms of the `MIT License <LICENSE>`__. This program is
-distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.
-
-The project is free to use for COMMERCIAL USE, MODIFICATION,
-DISTRIBUTION and PRIVATE USE as long as the original license is included
-as well as this copy right notice at the top of the modified files.
 """
 
 from cliconfig._version import __version__, __version_tuple__
